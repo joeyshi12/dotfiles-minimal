b5fc4602fcc8a70502115ce1f309f37b391c3d6a
joeyshi12 <joeyshi12@gmail.com>
2025-07-12 18:19:16 -0700

    removed bash_logout

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOTFILES_DIR=~/.dotfiles
-for file in .config/* .bash_profile .bashrc .bash_logout; do
+for file in .config/* .bash_profile .bashrc; do
     rm -f "${HOME}/$file"
     ln -sfn "${DOTFILES_DIR}/${file}" "${HOME}/${file}"
 done
